65990ad3634796d437ef8fdcd34d95b4e789c49f
mateus-kent <mateusquent2003@gmail.com>
2023-02-28 11:57:33 -0300

    feat: add auth middleware

--- src/helpers/tokenHelper.ts
+++ src/helpers/tokenHelper.ts
@@ -1,14 +1,23 @@
-import jwt from 'jsonwebtoken'
+import jwt, { JwtPayload } from 'jsonwebtoken'
 
 import { env } from '../env/'
+interface TokenPayload {
+ userId: string
+}
 
 class TokenHelper {
- generate(userId: number) {
-  return jwt.sign({ userId }, env.JWT_SECRET, { expiresIn: '1d' })
+ generate(userId: string) {
+  return jwt.sign({ sub: { userId } }, env.JWT_SECRET, { expiresIn: '1d' })
  }
 
  verify(token: string) {
-  return jwt.verify(token, env.JWT_SECRET)
+  const payload = jwt.verify(token, env.JWT_SECRET) as JwtPayload
+
+  if (!payload.sub) {
+   throw new Error('Invalid Jwt payload')
+  }
+
+  return payload.sub as unknown as TokenPayload
  }
 }
 
