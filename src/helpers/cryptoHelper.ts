0abb5bb784142dce12e95e45bf2e5407e42a7102
mateus-kent <mateusquent2003@gmail.com>
2023-03-01 23:39:11 -0300

    feat: create a crypto helper to crypt the credentials

---
+++ src/helpers/cryptoHelper.ts
@@ -0,0 +1,28 @@
+import crypto from 'crypto'
+
+import { env } from '../env'
+
+class CryptoHelper {
+ private static readonly algorithm = 'aes-256-cbc'
+ private static readonly iv = crypto.randomBytes(16)
+ private static readonly secretKey = env.ENCRYPTION_KEY
+
+ public static encrypt(text: string): string {
+  const cipher = crypto.createCipheriv(this.algorithm, this.secretKey, this.iv)
+  const encrypted = Buffer.concat([cipher.update(text), cipher.final()])
+
+  return `${this.iv.toString('hex')}:${encrypted.toString('hex')}`
+ }
+
+ public static decrypt(encryptedText: string): string {
+  const [ivHex, encryptedHex] = encryptedText.split(':')
+  const iv = Buffer.from(ivHex, 'hex')
+  const encrypted = Buffer.from(encryptedHex, 'hex')
+  const decipher = crypto.createDecipheriv(this.algorithm, this.secretKey, iv)
+  const decrypted = Buffer.concat([decipher.update(encrypted), decipher.final()])
+
+  return decrypted.toString()
+ }
+}
+
+export const cryptoHelper = new CryptoHelper()
