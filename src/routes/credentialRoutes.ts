95529e4410338a52c7fe2ffccf07806cdd4aca24
mateus-kent <mateusquent2003@gmail.com>
2023-03-03 11:53:59 -0300

    feat: create a useCase for delete and update credential

--- src/routes/credentialRoutes.ts
+++ src/routes/credentialRoutes.ts
@@ -1,7 +1,13 @@
 import express, { NextFunction, Request, Response } from 'express'
 
 import { authMiddleware } from '../middlewares/authMiddleware'
-import { createCredential, getCredentialById, getCredentialsByUser } from '../useCases/credentials'
+import {
+ createCredential,
+ deleteCredential,
+ getCredentialById,
+ getCredentialsByUser,
+ updateCredential
+} from '../useCases/credentials'
 
 export const credentialRouter = express.Router()
 
@@ -18,3 +24,15 @@ credentialRouter.get(
 )
 
 credentialRouter.get('/getCredential/:id', getCredentialById)
+
+credentialRouter.put(
+ '/updateCredential/:id',
+ (req: Request, res: Response, next: NextFunction) => authMiddleware.execute(req, res, next),
+ updateCredential
+)
+
+credentialRouter.delete(
+ '/deleteCredential/:id',
+ (req: Request, res: Response, next: NextFunction) => authMiddleware.execute(req, res, next),
+ deleteCredential
+)
