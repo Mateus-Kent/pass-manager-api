f3f7c138f3db677bffc39127ca34c3c42176f00e
mateus-kent <mateusquent2003@gmail.com>
2023-03-06 15:27:21 -0300

    fix: fix getUsers use case

--- src/useCases/users/getUsers.ts
+++ src/useCases/users/getUsers.ts
@@ -5,5 +5,5 @@ import * as UserService from '../../models/user'
 export async function getUsers(req: Request, res: Response) {
  const users = await UserService.getUsers()
 
- return users.length > 0 ? res.status(200).json(users) : res.status(404).send()
+ return res.status(200).json(users)
 }
